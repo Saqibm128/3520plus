cc818eab5aad328713aa8b3fdaff67bb35d3faf4
Saqib <saqibm128@gmail.com>
2017-12-20 13:49:08 -0500

    doing nullclines since they seem easiest to deal with

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 var express = require('express');
 var app = express();
+var path = require('path')
+var serveStatic = require("serve-static")
 
-app.use(epxress.static('static'))
+app.use(serveStatic(path.join(__dirname,"static")))
+console.log(path.join(__dirname,"static"))
+
+app.listen(3000)
---
+++ static/stoichiometric_matrix/main.js
@@ -0,0 +1,7 @@
+var num_ext = 6; //Exterior nodes i.e. number of arrows le. We assume that each. Exterior nodes have only one path in and out
+var num_int = 5; //Interior nodes
+var min_conn = 1;
+var max_conn = 5;
+$(document).ready(function(){
+  console.log("hi")
+});
